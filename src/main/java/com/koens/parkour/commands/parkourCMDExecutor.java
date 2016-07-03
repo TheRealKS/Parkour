3d0cf135fe70579ec363e94b239552c02e156a59
koens <koen.sauren@gmail.com>
2016-07-03 16:50:57 +0200

    Commited new stuff

--- src/main/java/com/koens/parkour/commands/parkourCMDExecutor.java
+++ src/main/java/com/koens/parkour/commands/parkourCMDExecutor.java
@@ -1,6 +1,9 @@
 package com.koens.parkour.commands;
 
 import com.koens.parkour.Parkour;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,13 +18,32 @@ public class parkourCMDExecutor implements CommandExecutor {
 
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (!(commandSender instanceof Player)) {
-            return false;
+            if (commandSender instanceof BlockCommandSender) {
+                BlockCommandSender sender = (BlockCommandSender) commandSender;
+                if (args[0].equalsIgnoreCase("start")) {
+                    startCMDExecutor executor = new startCMDExecutor(par);
+                    executor.run(getNearestPlayer(sender.getBlock().getLocation()));
+                }
+            }
         }
-        Player p = (Player) commandSender;
-        if (args[0].equalsIgnoreCase("start")) {
-            startCMDExecutor executor = new startCMDExecutor(par);
-            executor.run(p);
+        else {
+            Player p = (Player) commandSender;
+            if (args[0].equalsIgnoreCase("start")) {
+                startCMDExecutor executor = new startCMDExecutor(par);
+                executor.run(p);
+            }
         }
         return true;
+    }
+    private Player getNearestPlayer(Location loc) {
+        Player nearest = null;
+        Location initial = new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ());
+        double closestDist = initial.distanceSquared(loc);
+        for (Player i : Bukkit.getOnlinePlayers()) {
+            if (i.getLocation().distanceSquared(loc) < closestDist) {
+                nearest = i;
+            }
+        }
+        return nearest;
     }
 }
