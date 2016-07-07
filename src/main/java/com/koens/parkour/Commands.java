1177c4fe4733cbc2db4791d1c903752cab6a8fbb
koens <koen.sauren@gmail.com>
2016-07-07 09:14:28 +0200

    Commited new stuff

---
+++ src/main/java/com/koens/parkour/Commands.java
@@ -0,0 +1,18 @@
+package com.koens.parkour;
+
+public enum Commands {
+    START,
+    END,
+    CHECKPOINT,
+    RESET,
+    SELECT,
+    SELECTED,
+    ADD,
+    DELETE,
+    EDIT;
+
+    private enum editorCommands {
+        SETSTART,
+        SETEND;
+    }
+}
